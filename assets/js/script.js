2b50c1c38ea44a3cdcf407532b8dc3edffb60857
Xeless <tomdelintepro@gmail.com>
2024-04-19 15:00:05 +0200

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,6 +29,51 @@ document.addEventListener("DOMContentLoaded", function () {
         "https://fr.web.img4.acsta.net/pictures/19/09/16/17/09/4903250.jpg",
       genre: ["Action", "Drame", "Combat"],
     },
+
+    {
+        name: "Frieren: Beyond Journey's End",
+        auteur: "Kanehito Yamada",
+        picture:
+          "https://m.media-amazon.com/images/M/MV5BMjVjZGU5ZTktYTZiNC00N2Q1LThiZjMtMDVmZDljN2I3ZWIwXkEyXkFqcGdeQXVyMTUzMTg2ODkz._V1_QL75_UX190_CR0,2,190,281_.jpg 190w, https://m.media-amazon.com/images/M/MV5BMjVjZGU5ZTktYTZiNC00N2Q1LThiZjMtMDVmZDljN2I3ZWIwXkEyXkFqcGdeQXVyMTUzMTg2ODkz._V1_QL75_UX285_CR0,3,285,422_.jpg 285w, https://m.media-amazon.com/images/M/MV5BMjVjZGU5ZTktYTZiNC00N2Q1LThiZjMtMDVmZDljN2I3ZWIwXkEyXkFqcGdeQXVyMTUzMTg2ODkz._V1_QL75_UX380_CR0,4,380,562_.jpg 380w",
+        genre: ["Aventure", "Drame", "Combat"],
+      },
+
+      {
+        name: "My hero Academia",
+        auteur: "Kōhei Horikoshi",
+        picture:
+          "https://imgsrv.crunchyroll.com/cdn-cgi/image/format=auto,fit=contain,width=480,height=720,quality=85/catalog/crunchyroll/251524e3b5517b689317437d881eccf0.jpe",
+        genre: ["Aventure", "Action", "Combat"],
+      },
+
+      {
+        name: "Jujutsu Kaisen",
+        auteur: "Gege Akutam",
+        picture:
+          "https://imgsrv.crunchyroll.com/cdn-cgi/image/format=auto,fit=contain,width=480,height=720,quality=85/catalog/crunchyroll/e4e80c83c792d81c138e320874dbdffc.jpe",
+        genre: ["Aventure", "Action", "Combat"],
+      },
+      {
+        name: "Invincible",
+        auteur: "Robert Kirkman",
+        picture:
+          "https://www.comicsblog.fr/images/editeur/Arno/Novembre%202022/Invincible-poster-S2.jpg",
+        genre: ["Aventure", "Action", "Combat"],
+      },
+      {
+        name: "Arcane",
+        auteur: "Riot Games",
+        picture:
+          "https://fr.web.img6.acsta.net/pictures/21/11/02/11/12/2878509.jpg",
+        genre: ["Aventure", "Action", "Drame"],
+      },
+      {
+        name: "Spider-Man: Across the Spider-Verse",
+        auteur: "Brian Michael Bendis",
+        picture:
+          "https://fr.web.img2.acsta.net/pictures/23/05/11/10/00/1986933.jpg",
+        genre: ["Aventure", "Action", "Animation"],
+      },
   ];
 
   collections.forEach(function (collection) {
